305af623f22ed829d16c5108f47422f24a73678b
sskk224401-afk <ss.kk224401@gmail.com>
2026-07-30 14:12:24 +0300

    123

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,8 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
-// التعديل هنا: الاعتماد فقط على المتغير البيئي الآمن
 const apiKey = import.meta.env.VITE_FIREBASE_API_KEY;
 
 const firebaseConfig = {
@@ -19,5 +19,6 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
 export const googleProvider = new GoogleAuthProvider();
 export default app;
